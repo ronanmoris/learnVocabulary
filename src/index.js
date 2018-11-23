3f1c65434071ea119919e56505cd04ad9a2f5977
Ronan Moris <ronan.estima-moris@kis.krieger.de>
2018-11-23 17:47:02 +0100

    add file loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from "lodash";
+import './style.css';
+import Foto from './foto.jpg';
 
 function component() {
   let element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  var myFoto = new Image();
+  myFoto.src = Foto;
+
+  element.appendChild(myFoto);
 
   return element;
 }
