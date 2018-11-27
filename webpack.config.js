01083b638de4bcbcae78d668a2b980b727f01e69
Ronan Moris <ronan.estima-moris@kis.krieger.de>
2018-11-27 09:09:32 +0100

    set up webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    app: "./src/index.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: './dist'
   },
    module: {
      rules: [
