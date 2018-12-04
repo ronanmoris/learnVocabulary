121a2368f3f319fdc054edbfbfbbb4c595c22a78
ronanmoris <ronanmoris@gmail.com>
2018-12-04 20:10:23 +0100

    set up jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,39 @@
 const path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  entry: {
-    app: "./src/index.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devtool: "inline-source-map",
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-   module: {
-     rules: [
-       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: {
+        app: "./src/index.js"
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devtool: "inline-source-map",
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+        contentBase: "./dist",
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
         ]
-      },
-      {
-         test: /\.(png|svg|jpg|gif)$/,
-         use: [
-           'file-loader'
-         ]
-      }
-    ]
-  },
-  resolve: { // Do I need this?
-    extensions: ['*', '.js', '.jsx']
-  },
+    },
+    resolve: {
+    // Do I need this?
+        extensions: ["*", ".js", ".jsx"]
+    }
 };
